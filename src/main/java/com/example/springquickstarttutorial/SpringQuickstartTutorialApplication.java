ae2269e2c1f021032d9d2dfc4da4b1e2482f09ae
unknown <graham.flaspoehler@paradyme.us>
2020-05-14 11:48:25 -0400

    First commit

---
+++ src/main/java/com/example/springquickstarttutorial/SpringQuickstartTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springquickstarttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuickstartTutorialApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringQuickstartTutorialApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springquickstarttutorial/SpringQuickstartTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springquickstarttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuickstartTutorialApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
